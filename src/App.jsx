cbe76bf330f46b406887820d6795b5222bce7684
Asliddin Choriyev <asliddinchoriyev0828@gmail.com>
2024-01-13 17:36:59 +0500

    completed:

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import { useEffect, useState } from "react";
+import styles from "./App.module.css";
+import bgVideo from "../../../My Videos/for projects/golden.mp4";
+
+function App() {
+  const [time, setTime] = useState(new Date());
+  useEffect(() => {
+    setInterval(() => setTime(new Date()), 1000);
+  });
+  return (
+    <>
+      <video
+        className={styles["bgVideo"]}
+        src={bgVideo}
+        autoPlay
+        muted
+        loop
+      ></video>
+      <div className={styles["container"]}>
+        <span className={styles["before"]}></span>
+        <p className={styles["uz"]}>Uzbeksitan</p>
+        <h1 className={styles["time"]}>
+          {time.toLocaleTimeString().replace(/AM|PM/g, "")}
+        </h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
